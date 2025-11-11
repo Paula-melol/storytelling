1011b3beafbfd77c51eed1098d84b64a835c4547
Paula-melol <paula.melo@studentfr.ch>
2025-11-11 17:02:14 +0100

    debut story

--- src/js/main.js
+++ src/js/main.js
@@ -13,24 +13,44 @@ var timeLineNotification = gsap.timeline({
 timeLineNotification.to(".l-homepage-notification-1", {
   opacity: 1,
   scale: "1",
+  duration: 3,
 });
 timeLineNotification.to(".l-homepage-notification-2", {
   opacity: 1,
   scale: "1",
+  duration: 3,
 });
 timeLineNotification.to(".l-homepage-notification-3", {
   opacity: 1,
   scale: "1",
+  duration: 3,
 });
 timeLineNotification.to(".l-homepage-notification-4", {
   opacity: 1,
   scale: "1",
+  duration: 3,
 });
 
 timeLineNotification.to(".l-homepage-notification-5", {
   opacity: 1,
   scale: "1",
+  duration: 3,
 });
+
+timeLineNotification.to(
+  ".l-homepage-notification-1, .l-homepage-notification-2, .l-homepage-notification-3,.l-homepage-notification-4,.l-homepage-notification-5",
+  {
+    opacity: 0,
+    scale: "0",
+    duration: 1,
+  }
+);
+timeLineNotification.to(".l-homepage-title", {
+  opacity: 0,
+  scale: "0",
+  duration: 1,
+});
+
 timeLineNotification.to(".l-homepage-flash", {
   opacity: 1,
   scale: "1",
