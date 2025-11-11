dc857bb127bfcba545b5a11bcafbb9e1d63c9d15
Paula-melol <paula.melo@studentfr.ch>
2025-11-11 14:30:22 +0100

    animation js homepage

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+var timeLineNotification = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".l-homepage",
+    scrub: 1,
+    pin: true,
+  },
+});
+
+timeLineNotification.to(".l-homepage-notification-1", {
+  opacity: 1,
+  scale: "1",
+});
+timeLineNotification.to(".l-homepage-notification-2", {
+  opacity: 1,
+  scale: "1",
+});
+timeLineNotification.to(".l-homepage-notification-3", {
+  opacity: 1,
+  scale: "1",
+});
+timeLineNotification.to(".l-homepage-notification-4", {
+  opacity: 1,
+  scale: "1",
+});
+
+timeLineNotification.to(".l-homepage-notification-5", {
+  opacity: 1,
+  scale: "1",
+});
+timeLineNotification.to(".l-homepage-flash", {
+  opacity: 1,
+  scale: "1",
+});
+
+// animation homepage
